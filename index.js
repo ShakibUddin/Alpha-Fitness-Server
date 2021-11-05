1a3e2146e15b84011e56d41816aa9e1508e37d3f
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-11-06 03:59:55 +0600

    deleted special classes route

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ async function run() {
         const database = client.db(process.env.DB_NAME);
         const productsCollection = database.collection('products');
         const trainingsCollection = database.collection('trainings');
+        const storiesCollection = database.collection('stories');
+        const specialClassesCollection = database.collection('special-classes');
         const successesCollection = database.collection('successes');
+        const membershipsCollection = database.collection('membership');
         const usersCollection = database.collection('users');
+        const queriesCollection = database.collection('queries');
 
         // GET API
         app.get('/products', async (req, res) => {
@@ -37,29 +41,39 @@ async function run() {
             res.send(trainings);
         });
         // GET API
+        app.get('/stories', async (req, res) => {
+            const cursor = storiesCollection.find({});
+            const stories = await cursor.toArray();
+            res.send(stories);
+        });
+        // GET API
+        app.get('/memberships', async (req, res) => {
+            const cursor = membershipsCollection.find({});
+            const memberships = await cursor.toArray();
+            res.send(memberships);
+        });
+        // GET API
         app.get('/successes', async (req, res) => {
             const cursor = successesCollection.find({});
             const successes = await cursor.toArray();
             res.send(successes);
         });
-        // POST API(Check if a user exists in database)
-        app.post('/checkusers', async (req, res) => {
-            // Query for a users email
-            const query = { email: req.body.email };
-            const user = await usersCollection.findOne(query);
-            if (user) {
+
+        // POST API
+        app.post('/users', async (req, res) => {
+            const data = req.body;
+            const insertOperation = await usersCollection.insertOne(data);
+            if (insertOperation.acknowledged) {
                 res.send(true);
             }
             else {
                 res.send(false);
             }
-
         });
-        // POST API(Check if a user exists in database)
-        app.post('/users', async (req, res) => {
-            // Query for a users email
+        // POST API
+        app.post('/queries', async (req, res) => {
             const data = req.body;
-            const insertOperation = await usersCollection.insertOne(data);
+            const insertOperation = await queriesCollection.insertOne(data);
             if (insertOperation.acknowledged) {
                 res.send(true);
             }
